7be74172b96af8fd06cce4469a799b339530f516
MatthewPCope <matthew.p.cope@gmail.com>
2024-03-23 10:11:54 -0700

    added overview

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import Tilt from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt >
+
+    </Tilt>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p variants={fadeIn("", "", 0.1, 1)}
+      className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+      Transitioning from digital audio and video production into full-stack web development, I bring creativity, humor, and a readiness to tackle challenges. My approach to work is grounded in being genuine and reliable, ensuring I contribute more than just code. Eager to grow and learn, I aim to add a human touch to the digital world, keeping the atmosphere light and engaging. Let's connect and see how we can make a meaningful impact together.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
   )
 }
 
