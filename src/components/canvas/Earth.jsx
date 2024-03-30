25fedcefe54b58e0547ee451e40ee085fb3f152e
MatthewPCope <matthew.p.cope@gmail.com>
2024-03-30 07:46:59 -0700

    starting earth model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
 const Earth = () => {
   return (
     <div>Earth</div>
   )
 }
+
+
+
 
 export default Earth
